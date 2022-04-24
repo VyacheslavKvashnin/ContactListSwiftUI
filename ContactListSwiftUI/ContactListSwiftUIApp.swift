293c16a664c63c56f636aeb31256659d1d89368e
komplektan <54449303+komplektan@users.noreply.github.com>
2022-04-24 22:49:01 +0500

    Initial Commit

---
+++ ContactListSwiftUI/ContactListSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSwiftUIApp.swift
+//  ContactListSwiftUI
+//
+//  Created by Вячеслав Квашнин on 24.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
